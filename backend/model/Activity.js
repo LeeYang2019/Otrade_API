fa65986d03fc7bbb906c1cd66ffb2dff868a8c38
leeyang2019 <nyang@madisoncollege.edu>
2020-11-24 15:38:43 -0500

    refactored code

--- backend/model/Activity.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-//define schema
-const ActivitySchema = mongoose.Schema({});
-
-//export schema as model
-module.exports = mongoose.model('Activity', ActivitySchema);
